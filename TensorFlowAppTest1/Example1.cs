d7d1b2393a53e989d66b1605e7d9bca7c2f03e2d
pablo guachichullca <pablo.ab34@hotmail.com>
2023-08-30 11:35:59 -0500

    Agregar archivos de proyecto.

---
+++ TensorFlowAppTest1/Example1.cs
@@ -0,0 +1,14 @@
+﻿using NumSharp;
+using Tensorflow;
+using static Tensorflow.Binding;
+
+namespace TensorFlowAppTest1
+{
+    public class Example1
+    {
+        public void execute()
+        {
+            
+    }       
+    }
+}
---
+++ TensorFlowAppTest1/IExample.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tensorflow;
+
+namespace TensorFlowAppTest1
+{
+    public interface IExample
+    {
+        /// <summary>
+        /// True to run example
+        /// </summary>
+        bool Enabled { get; set; }
+
+        /// <summary>
+        /// Set true to import the computation graph instead of building it.
+        /// </summary>
+        bool IsImportingGraph { get; set; }
+
+        string Name { get; }
+
+        bool Run();
+
+        /// <summary>
+        /// Build dataflow graph, train and predict
+        /// </summary>
+        /// <returns></returns>
+        void Train(Session sess);
+        void Test(Session sess);
+
+        void Predict(Session sess);
+
+        Graph ImportGraph();
+
+        Graph BuildGraph();
+
+        /// <summary>
+        /// Prepare dataset
+        /// </summary>
+        void PrepareData();
+
+    }
+}
